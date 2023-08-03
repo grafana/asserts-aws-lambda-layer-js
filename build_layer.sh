e96ee51dacc57ec737046550d1b414db4674f341
jradhakrishnan <radhakrishnan.jankiraman@gmail.com>
2023-08-03 18:02:31 +0530

    Disable cli pagination

--- build_layer.sh
+++ build_layer.sh
@@ -9,10 +9,10 @@ sed -i '' "s/$COMMIT_HASH/latest/g" package.json
 
 aws s3 cp "asserts-aws-lambda-layer-js-$COMMIT_HASH.zip" s3://asserts-lambda-layers
 
-LAYER_VERSION=$(aws lambda publish-layer-version --output json --layer-name asserts-aws-lambda-layer-js \
+LAYER_VERSION=$(aws lambda publish-layer-version --no-paginate --no-cli-pager --output json --layer-name asserts-aws-lambda-layer-js \
   --description "Asserts AWS Lambda Layer for NodeJS created from asserts-aws-lambda-layer-js-$COMMIT_HASH" \
   --content "S3Bucket=asserts-lambda-layers,S3Key=asserts-aws-lambda-layer-js-$COMMIT_HASH.zip" | jq '.Version')
 export LAYER_VERSION
 
-aws lambda add-layer-version-permission --layer-name asserts-aws-lambda-layer-js \
+aws lambda add-layer-version-permission --no-paginate --no-cli-pager --layer-name asserts-aws-lambda-layer-js \
   --statement-id ReadPublic --action lambda:GetLayerVersion  --principal "*" --version-number "${LAYER_VERSION}" --output json
